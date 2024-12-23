0e8c4ba654e39b07a4c56489acda224f4a1a5f7a
Peter Harris <peter.harris@arm.com>
2024-12-23 22:00:40 +0000

    Commit empty performance layer

---
+++ layer_gpu_performance/source/device.cpp
@@ -0,0 +1,97 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * ----------------------------------------------------------------------------
+ * Copyright (c) 2024 Arm Limited
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ * ----------------------------------------------------------------------------
+ */
+
+#include <array>
+#include <iostream>
+#include <fstream>
+#include <sys/stat.h>
+#include <vector>
+
+#include "framework/utils.hpp"
+
+#include "device.hpp"
+#include "instance.hpp"
+
+/**
+ * @brief The dispatch lookup for all of the created Vulkan instances.
+ */
+static std::unordered_map<void*, std::unique_ptr<Device>> g_devices;
+
+/* See header for documentation. */
+void Device::store(
+    VkDevice handle,
+    std::unique_ptr<Device> device
+) {
+    void* key = getDispatchKey(handle);
+    g_devices.insert({ key, std::move(device) });
+}
+
+/* See header for documentation. */
+Device* Device::retrieve(
+    VkDevice handle
+) {
+    void* key = getDispatchKey(handle);
+    assert(isInMap(key, g_devices));
+    return g_devices.at(key).get();
+}
+
+/* See header for documentation. */
+Device* Device::retrieve(
+    VkQueue handle
+) {
+    void* key = getDispatchKey(handle);
+    assert(isInMap(key, g_devices));
+    return g_devices.at(key).get();
+}
+
+/* See header for documentation. */
+Device* Device::retrieve(
+    VkCommandBuffer handle
+) {
+    void* key = getDispatchKey(handle);
+    assert(isInMap(key, g_devices));
+    return g_devices.at(key).get();
+}
+
+/* See header for documentation. */
+void Device::destroy(
+    Device* device
+) {
+    g_devices.erase(getDispatchKey(device));
+}
+
+/* See header for documentation. */
+Device::Device(
+    Instance* _instance,
+    VkPhysicalDevice _physicalDevice,
+    VkDevice _device,
+    PFN_vkGetDeviceProcAddr nlayerGetProcAddress
+):
+    instance(_instance),
+    physicalDevice(_physicalDevice),
+    device(_device)
+{
+    initDriverDeviceDispatchTable(device, nlayerGetProcAddress, driver);
+}
