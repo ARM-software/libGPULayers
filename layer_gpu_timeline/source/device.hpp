72849056ffaa259a7ad44ec378e32f42dac49792
Peter Harris <peter.harris@arm.com>
2025-01-20 21:39:01 +0000

    Fix copyright years

--- layer_gpu_timeline/source/device.hpp
+++ layer_gpu_timeline/source/device.hpp
@@ -1,7 +1,7 @@
 /*
  * SPDX-License-Identifier: MIT
  * ----------------------------------------------------------------------------
- * Copyright (c) 2024 Arm Limited
+ * Copyright (c) 2024-2025 Arm Limited
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
